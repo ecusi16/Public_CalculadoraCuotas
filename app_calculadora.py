c1b1e93ad1906819daa6fad997afdca7f2ccd05a
EvelynCL <evelyn.cusi16@gmail.com>
2024-05-31 16:26:07 -0400

    Cambiar codigo fuente

--- app_calculadora.py
+++ app_calculadora.py
@@ -4,7 +4,7 @@ from calculadora_cuotas import obtener_cuota_mensual_total, \
 
 
 # Crear la interfaz de usuario
-st.title("Calculadora de Cuotas de Préstamo")
+'''st.title("Calculadora de Cuotas de Préstamo")
 
 monto = st.number_input("Monto del préstamo", min_value=0.0, step=1000.0)
 plazo = st.number_input("Plazo en meses", min_value=6, step=6)
@@ -19,4 +19,6 @@ if st.button("Calcular cuota mensual"):
     
     with st.expander("Detalles de la cuota"):
         for key, value in detalles.items():
-            st.write(f"{key}: ${value:.2f}")
+            st.write(f"{key}: ${value:.2f}")'''
+st.title("Calculadora de Cuotas de Préstamo")
+st.write("Hello world")
